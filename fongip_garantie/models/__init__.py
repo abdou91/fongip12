a1206e4a2349f7c74088c85421e2cab9177b4229
Abdou Mbar Ly <abdoumbarly@gmail.com>
2022-09-21 12:43:01 +0000

    delete garantie module

--- fongip_garantie/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-#from . import models
-#from . import fongip_rapport
-from . import project
-from . import credit
-from . import res_partner
-from . import sous_fonds
-from . import commission
-from . import utility
